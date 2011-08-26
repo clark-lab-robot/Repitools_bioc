47eb931596c60b9f077533a316bb34fa3521cb26
d.strbenac <d.strbenac@bc3139a8-67e5-0310-9ffc-ced21a209358>
2011-08-26 00:50:16 +0000

    closeClusterFeatures code simplification
    git-svn-id: https://hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/Repitools@57667 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/closeClusterFeatures.R
+++ R/closeClusterFeatures.R
@@ -1,43 +1,32 @@
 setGeneric("closeClusterFeatures", function(c.list, ...){standardGeneric("closeClusterFeatures")})
 
-.clusterBlocksInds <- function(cluster.IDs.chr, n.consec)
+.clusterBlocks <- function(tx.df, cluster.IDs, n.consec)
 {
-    id.levels <- sort(unique(cluster.IDs.chr))
-    blocks.inds.chr <- lapply(id.levels, function(x)
-    {
-        which.id <- IRanges(which(cluster.IDs.chr == x), width = 1)
-        which.in.blocks <- reduce(which.id)
-        which.in.blocks <- which.in.blocks[width(which.in.blocks) >= 3]
-        which.dispersed <- reduce(which.id, min.gapwidth = 2)
-        unique(which.dispersed[subjectHits(findOverlaps(which.in.blocks, which.dispersed))])
-    })
-    names(blocks.inds.chr) <- id.levels
-    blocks.inds.chr
+    anno.cols <- numeric()
+    if(ncol(tx.df) > 5) anno.cols <- 6:ncol(tx.df)
+    blocks <- unlist(tapply(cluster.IDs, tx.df[, "seqnames"], function(x)
+                    {
+                        id.levels <- sort(unique(x))
+                        chr.blocks <- lapply(id.levels, function(y)
+                        {
+                            which.id <- IRanges(as.numeric(names(which(x == y))), width = 1)
+                            which.consec <- reduce(which.id)
+                            which.consec <- which.consec[width(which.consec) >= 3]
+                            which.dispersed <- reduce(which.id, min.gapwidth = 2)
+                            blocks.inds <- as.list(unique(which.dispersed[subjectHits(findOverlaps(which.consec, which.dispersed))]))
+                            sapply(blocks.inds, function(z)
+                            {
+                                cluster.block <- GRanges(tx.df[z, "seqnames"],
+                                                         IRanges(tx.df[z, "start"], tx.df[z, "end"]), 
+                                                         tx.df[z, "strand"])
+                                values(cluster.block) <- DataFrame(tx.df[z, anno.cols], cluster = cluster.IDs[z])
+                                cluster.block
+                            })
+                        })
+                    }), use.names = FALSE)
 }
 
-.clusterBlocks <- function(tx.df, cluster.IDs, name.col, ...)
-{
-    anno.cols <- colnames(tx.df) %in% c("gene", "gene.symbol")
-    chr.blocks.inds <- tapply(cluster.IDs, tx.df[, "seqnames"], function(x) .clusterBlocksInds(x, ...))
-    blocks.inds <- unlist(mapply(function(clust.blocks, chr)
-    {
-        which.on.chr <- which(tx.df[, "seqnames"] == chr)
-        lapply(clust.blocks, function(blocks)
-        {
-            sapply(as.list(blocks), function(block)
-                                    {
-                                        feat.inds <- which.on.chr[block]
-                                        feat.locs <- GRanges(tx.df[feat.inds, "seqnames"],
-                                                             IRanges(tx.df[feat.inds, "start"], tx.df[feat.inds, "end"]), 
-                                                             tx.df[feat.inds, "strand"])
-                                        values(feat.locs) <- DataFrame(feature.id = tx.df[feat.inds, 5 + name.col], cluster = cluster.IDs[feat.inds])
-                                        feat.locs
-                                    })
-        })
-    }, chr.blocks.inds, names(chr.blocks.inds)))
-}
-
-setMethod("closeClusterFeatures", "ClusteredScoresList", function(c.list, n.consec = NULL, n.perm = 100, name.col = NULL, verbose = TRUE)
+setMethod("closeClusterFeatures", "ClusteredScoresList", function(c.list, n.consec = NULL, n.perm = 100, verbose = TRUE)
 {
     if(is.null(n.consec))
         stop("'n.consec' not given.")
@@ -48,17 +37,18 @@ setMethod("closeClusterFeatures", "ClusteredScoresList", function(c.list, n.cons
     tx.order <- order(tx.df[, "seqnames"], pos)
     tx.df <- tx.df[tx.order, ]
     cluster.IDs <- cluster.IDs[tx.order]
+    names(cluster.IDs) <- 1:length(cluster.IDs)
     
     if(verbose)
         message("Finding blocks in experimental clusters.")
-    blocks <- .clusterBlocks(tx.df, cluster.IDs, name.col, n.consec)
+    blocks <- .clusterBlocks(tx.df, cluster.IDs, n.consec)
 
     if(verbose)
         message("Finding blocks in randomly ordered clusters.")
     rand.blocks <- lapply(1:n.perm, function(x)
                          {
                              rand.IDs <- sample(cluster.IDs, length(cluster.IDs))
-                             .clusterBlocks(tx.df, rand.IDs, name.col, n.consec)
+                             .clusterBlocks(tx.df, rand.IDs, n.consec)
                          })
     
     list(blocks = blocks, p.value = length(which(sapply(rand.blocks, length) >= length(blocks))) / n.perm)
