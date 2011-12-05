f7939187c5e08f44b2e638d070dcff71d252ce30
d.strbenac <d.strbenac@bc3139a8-67e5-0310-9ffc-ced21a209358>
2011-12-05 00:16:50 +0000

    mappabilityCalc has simpler edge-handling code. GRanges signature now takes a window argument.
    git-svn-id: https://hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/Repitools@61113 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/mappabilityCalc.R
+++ R/mappabilityCalc.R
@@ -1,10 +1,19 @@
 setGeneric("mappabilityCalc", function(x, ...){standardGeneric("mappabilityCalc")})
     
-setMethod("mappabilityCalc", "GRanges", function(x, organism, verbose = TRUE)
+setMethod("mappabilityCalc", "GRanges", function(x, organism, window = NULL,
+          verbose = TRUE)
 {
     require(GenomicRanges)
 
     if(verbose) message("Calculating mappability.")
+
+    if(!is.null(window))
+    {
+        x.posns <- IRanges(as.numeric(ifelse(strand(x) == '+', start(x), end(x))),
+                           width = 1)
+        ranges(x) <- x.posns
+        x <- resize(x, window, "center")
+    }
     strand(x) <- "+"
     chrs <- as.character(seqnames(x))
     regions.by.chr <- split(x, chrs)
@@ -14,39 +23,20 @@ setMethod("mappabilityCalc", "GRanges", function(x, organism, verbose = TRUE)
         # Handle case of windows overlapping past ends of chromosome.
         chr.name <- as.character(seqnames(y))[1]
         chr.max <- length(organism[[chr.name]])
-        which.outside.start <- end(y) < 1
-        which.outside.end <- start(y) > chr.max
-        which.in.chr <- !which.outside.start & !which.outside.end
-        which.past.start <- start(y) < 1 & which.in.chr
-        which.past.end <- end(y) > chr.max & which.in.chr
-        start.Ns <- -start(y)[which.past.start] + 1
-        end.Ns <- end(y)[which.past.end] - chr.max
-        start(y)[which.past.start] = 1
-        end(y)[which.past.end] = chr.max
-        temp <- character()
-        temp[which.in.chr] <- getSeq(organism, y[which.in.chr], as.character = TRUE)
-        temp[!which.in.chr] <- 'N'
-        temp[which.past.start] <- unlist(mapply(function(y, z)
-                                  {
-                                     paste(paste(rep('N', z), collapse = ''), y, sep = '')
-                                  }, temp[which.past.start], start.Ns))
-        temp[which.past.end] <- unlist(mapply(function(y, z)
-                                {
-                                    paste(y, paste(rep('N', z), collapse = ''), sep = '')
-                                }, temp[which.past.end], end.Ns))
-        
-        tempAlphabet <- alphabetFrequency(DNAStringSet(temp), as.prob = TRUE)
-        1 - tempAlphabet[, "N"]
+        inside.regions <- restrict(y, 1, chr.max, keep.all.ranges = TRUE)
+        window.seqs <- getSeq(organism, inside.regions)
+        unmap.counts <- alphabetFrequency(DNAStringSet(window.seqs))[, 'N']
+        unmap.counts <- unmap.counts + width(y) - width(inside.regions)
+        1 - (unmap.counts / width(y))
     })
     unsplit(mappability.by.chr, chrs)
 })
     
-setMethod("mappabilityCalc", "data.frame", function(x, organism, window = NULL, ...)
+setMethod("mappabilityCalc", "data.frame", function(x, organism, ...)
 {
     require(GenomicRanges)
 
     if (is.null(x$position)) x$position <- ifelse(x$strand == '+', x$start, x$end)
     x <- GRanges(x$chr, IRanges(x$position, width = 1))
-    if(!is.null(window)) x <- resize(x, window, "center")
     mappabilityCalc(x, organism, ...)
 })
